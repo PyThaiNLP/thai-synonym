fd9453ddd56f54955360c0e20eec2f13c6b94f72
Theppitak Karoonboonyanan <theppitak@gmail.com>
2022-07-20 22:53:39 +0700

    gen-thesaurus.py: Get input/output file from command line

--- utils/gen-thesaurus.py
+++ utils/gen-thesaurus.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 import csv
+import sys
+
+DEFAULT_DAT = "th_th_TH_v2.dat"
+
+def usage():
+	print("Usage: {} <csv-data-file> [<out-dat-file>]"
+	        .format(sys.argv[0]))
+	print("If omitted, <out-dat-file> defaults to {}."
+	        .format(DEFAULT_DAT))
+
+if len(sys.argv) not in [2, 3]:
+	usage()
+	sys.exit()
+data_csv = sys.argv[1]
+out_dat = DEFAULT_DAT
+if len(sys.argv) == 3:
+	out_dat = sys.argv[2]
 
 dat = {}
-with open("data.csv", "r", encoding="utf-8-sig") as csv_file:
+with open(data_csv, "r", encoding="utf-8-sig") as csv_file:
 	csv_reader = csv.reader(csv_file, delimiter=',')
 	next(csv_reader, None)
 	for row in csv_reader:
@@ -18,7 +35,7 @@ THES_POS = {
 	"adv" : "ว.",
 }
 
-with open("th_th_TH_v2.dat", "w", encoding="utf-8") as out:
+with open(out_dat, "w", encoding="utf-8") as out:
 	out.write("UTF-8\n")
 	for word in dat:
 		out.write("{}|{}\n".format(word, len(dat[word])))
